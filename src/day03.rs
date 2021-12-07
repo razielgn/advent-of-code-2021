ca0d9f6dbbd74107531ac4b8c60b8c26724f3a28
Federico Ravasio <ravasio.federico@gmail.com>
2021-12-07 21:20:55 +0000

    day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -36,8 +36,51 @@ pub fn part1(input: &[String]) -> u32 {
 }
 
 #[aoc(day3, part2)]
-pub fn part2(_input: &[String]) -> u32 {
-    todo!()
+pub fn part2(input: &[String]) -> u32 {
+    fn find_oxygen_generator_rating(mut input: Vec<String>) -> u32 {
+        for bit in 0.. {
+            if input.len() == 1 {
+                return u32::from_str_radix(&input[0], 2).unwrap();
+            }
+
+            let ones = itertools::partition(&mut input, |s| s.chars().nth(bit).unwrap() == '1');
+            let zeroes = input.len() - ones;
+            let split_index = ones;
+
+            if ones >= zeroes {
+                let _ = input.split_off(split_index);
+            } else {
+                input = input.split_off(split_index);
+            }
+        }
+
+        unreachable!()
+    }
+
+    fn find_co2_scrubber_rating(mut input: Vec<String>) -> u32 {
+        for bit in 0.. {
+            if input.len() == 1 {
+                return u32::from_str_radix(&input[0], 2).unwrap();
+            }
+
+            let zeroes = itertools::partition(&mut input, |s| s.chars().nth(bit).unwrap() == '0');
+            let ones = input.len() - zeroes;
+            let split_index = zeroes;
+
+            if ones >= zeroes {
+                let _ = input.split_off(split_index);
+            } else {
+                input = input.split_off(split_index);
+            }
+        }
+
+        unreachable!()
+    }
+
+    let oxygen_generator_rating = find_oxygen_generator_rating(input.to_vec());
+    let co2_scrubber_rating = find_co2_scrubber_rating(input.to_vec());
+
+    oxygen_generator_rating * co2_scrubber_rating
 }
 
 #[cfg(test)]
@@ -70,16 +113,16 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn example2() {
-    //     assert_eq!(part2(&input_generator(EXAMPLE)), 900);
-    // }
+    #[test]
+    fn example2() {
+        assert_eq!(part2(&input_generator(EXAMPLE)), 230);
+    }
 
-    // #[test]
-    // fn solution2() {
-    //     assert_eq!(
-    //         part2(&input_generator(include_str!("../input/2021/day3.txt"))),
-    //         1_942_068_080,
-    //     );
-    // }
+    #[test]
+    fn solution2() {
+        assert_eq!(
+            part2(&input_generator(include_str!("../input/2021/day3.txt"))),
+            793_873,
+        );
+    }
 }
